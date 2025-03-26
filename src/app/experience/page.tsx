8fca97faf63a15192cd0b908bcdbb412cd63bb0c
code-huddle-haider-jalal <haider.jalal@code-huddle.com>
2025-03-26 23:58:48 +0500

    initial files addded

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Experience() {
+  return (
+    <p>experience</p>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Services() {
+  return (
+    <p>Services</p>
+  );
+}
