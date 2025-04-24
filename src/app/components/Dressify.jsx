6edcb3bebd268bc18204abaca43a9512d64d35ca
Haider-Jalal <haider.jalal@code-huddle.com>
2025-04-24 12:11:54 +0500

    spline set

--- src/app/components/Dressify.jsx
+++ src/app/components/Dressify.jsx
@@ -7,10 +7,7 @@ export default function Dressify() {
       {/* Your Spline scene */}
       <Spline
         scene="https://prod.spline.design/JfpZqiCo0zZ2mLXn/scene.splinecode" 
-        width={1920}
-        height={1098}
       />
-
       {/* Black gradient at the top of the section */}
       <div className="absolute inset-x-0 top-0 h-1/6 bg-gradient-to-b from-black to-transparent z-20"></div>
       <div className="absolute inset-x-0 top-5 h-1/6 bg-gradient-to-b from-black to-transparent z-20"></div>
